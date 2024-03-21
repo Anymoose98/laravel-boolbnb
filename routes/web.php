38ef41f31d5e3718acd7fb19d6dd8075dd004feb
SquallStrifeX <squallstrifex@gmail.com>
2024-03-21 17:45:56 +0100

    add route

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::resource('apartments', ApartmentsController::class)->middleware('auth');
+
+
 require __DIR__.'/auth.php';
