2fcf73c51a6362a5071be45cc2a34fc30097ef17
Shamble94 <fratus.nicholas@gmail.com>
2024-03-21 17:41:56 +0100

    commit

---
+++ app/Models/Apartments.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Apartments extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'description',
+        'rooms',
+        'beds',
+        'bathrooms',
+        'square_meters',
+        'location',
+        'image',
+        'visibility',
+        'longitudine',
+        'latitudine',
+        
+    ];
+}
