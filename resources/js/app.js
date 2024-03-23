898fff95c79654393f407d9d98a60ff3a7c21e93
SquallStrifeX <squallstrifex@gmail.com>
2024-03-23 17:45:35 +0100

    commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,7 @@ deleteButtons.forEach((button) => {
         let url = `${window.location.origin}/apartments/${id}`;
        console.log(url)
         let form_delete = document.getElementById("form_delete");
-        
+
         form_delete.setAttribute("action", url)
     });
 
@@ -25,16 +25,16 @@ deleteButtons.forEach((button) => {
 const myButton = document.getElementById('myButton')
 // myButton.myButton.addEventListener('click' 'chiamataApi');{
     axios.get('https://api.tomtom.com/search/2/geocode/Via%20delle%20Baleniere%2070.json?key=GQoylkWTb8A3X4kupHH9BTdJj1GJaVKo')
-    console.log("prova")
+
     .then(response => {
         const data = response.data;
         const lat = data.results[0].position.lat;
         const lon = data.results[0].position.lon;
-    
+        console.log("prova")
         console.log("Latitudine:", lat);
         console.log("Longitudine:", lon);
-    
-       
+
+
     });
 
-    
+
