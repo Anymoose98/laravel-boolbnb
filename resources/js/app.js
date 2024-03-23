ab99ef9adfd773309257e29e2901207356881a93
Shamble94 <fratus.nicholas@gmail.com>
2024-03-23 14:17:51 +0100

    modale non funzionante

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,12 +10,12 @@ const deleteButtons = document.querySelectorAll(".btn-danger");
 deleteButtons.forEach((button) => {
     button.addEventListener("click", function(){
         let id = button.getAttribute("data-id");
-    
+        
         let url = `${window.location.origin}/apartments/${id}`;
-        console.log(url)
+       
 
         let form_delete = document.getElementById("form_delete");
-
+        console.log(form_delete)
         form_delete.setAttribute("action", url)
         
 
