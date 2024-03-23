3c3cde239e8044d0aeb93da98d7403bae8fc00c3
nicolocamassa <camassanicolo03@gmail.com>
2024-03-23 16:22:45 +0100

    [UPDATE] column for houses visualization added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,36 @@
 @section('content')
     <div class="image-carousel-container">
         <img src="{{ asset('img/carousel-1.jpg') }}" alt="ciao">
+
+        <div class="search-bar">
+
+        </div>
     </div>
+
+    <div class="cards-container">
+        <div class="row">
+            <div class="col">
+                <div class="personal-content">
+                    
+                </div>
+            </div>
+            <div class="col">
+                <div class="personal-content">
+                    
+                </div>
+            </div>
+            <div class="col">
+                <div class="personal-content">
+                    
+                </div>
+            </div>
+            <div class="col">
+                <div class="personal-content">
+                    
+                </div>
+            </div>
+
+        </div>
+    </div>
+    
 @endsection
