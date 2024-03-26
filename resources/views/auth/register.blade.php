6618d7c96c704ec0d3d317da51e19866b6f1ac18
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-03-26 16:55:47 +0100

    add * red

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="mb-4 row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Nome*</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Nome<span class="text-danger">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="mb-4 row">
-                            <label for="surname" class="col-md-4 col-form-label text-md-right">Cognome*</label>
+                            <label for="surname" class="col-md-4 col-form-label text-md-right">Cognome<span class="text-danger">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="mb-4 row">
-                            <label for="birth_date" class="col-md-4 col-form-label text-md-right">Data di nascita*</label>
+                            <label for="birth_date" class="col-md-4 col-form-label text-md-right">Data di nascita<span class="text-danger">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="birth_date" type="date" class="form-control @error('birth_date') is-invalid @enderror" name="birth_date" value="{{ old('birth_date') }}" required autocomplete="birth_date" autofocus>
@@ -56,7 +56,7 @@
                         
 
                         <div class="mb-4 row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Indirizzo email*</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Indirizzo email<span class="text-danger">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -70,7 +70,7 @@
                         </div>
 
                         <div class="mb-4 row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password*</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Password<span class="text-danger">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -84,7 +84,7 @@
                         </div>
 
                         <div class="mb-4 row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Conferma password*</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Conferma password<span class="text-danger">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
