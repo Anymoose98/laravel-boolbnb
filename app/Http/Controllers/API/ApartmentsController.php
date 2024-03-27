ada3ea409e8ba12ec82649744befe6b932aa0a7f
nicolocamassa <camassanicolo03@gmail.com>
2024-03-27 19:21:32 +0100

    [UPDATE] search for city added

--- app/Http/Controllers/API/ApartmentsController.php
+++ app/Http/Controllers/API/ApartmentsController.php
@@ -8,10 +8,33 @@ use App\Http\Controllers\Controller;
 
 class ApartmentsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $apartments = Apartments::all();
-
+        $query = Apartments::query();
+    
+        /* RECUPERO DEI DATI PRESI DAL JS */
+        if ($request->has(['latitude', 'longitude', 'radius'])) {
+            $latitude = $request->input('latitude');
+            $longitude = $request->input('longitude');
+            $radius = $request->input('radius');;
+    
+            /* CALCOLO DEL RANGE DELLA DISTANZA */
+            /* UN GRADO DI LONGITUDINE DOVREBBE ESSERE 111.12 [IMPRECISO] */
+            $approximateRange = $radius / 111.12; 
+    
+            /* CALCOLO APPROSSIMATIVO PER LATITUIDE E LONGITUDINE */
+            $latMin = $latitude - $approximateRange;
+            $latMax = $latitude + $approximateRange;
+            $lonMin = $longitude - $approximateRange;
+            $lonMax = $longitude + $approximateRange;
+    
+            /* QUERY CHE RICHIEDE GLI APPARTAMENTI ENTRO IL RANGE CALCOLATO */
+            $query->whereBetween('latitude', [$latMin, $latMax])
+                  ->whereBetween('longitude', [$lonMin, $lonMax]);
+        }
+    
+        $apartments = $query->get();
+    
         return response()->json([
             'success' => true,
             'results' => $apartments,
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,213 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="image-carousel-container">
+        <img src="{{ asset('img/carousel-1.jpg') }}" alt="ciao">
 
-<div class="image-carousel-container">
-    <img src="{{ asset('img/carousel-1.jpg') }}" alt="ciao">
+        <div class="search-bar">
+            <div class="search-elem">
+                <label for="city-input">Dove</label>
+                <input type="text" name="city-input" id="city-input" placeholder="Cerca la destinazione">
+            </div>
 
-    <div class="search-bar">
-        <div class="search-elem">
-            <label for="city-input">Dove</label>
-            <input type="text" name="city-input" id="city-input" placeholder="Cerca la destinazione">
+            <div class="search-elem">
+                <label for="guest-input">Quante persone</label>
+                <input type="text" name="guest-input" id="guest-input" placeholder="Inserisci il numero di ospiti">
+            </div>
+
+            <div class="search-elem">
+                <label for="city-input">Demo</label>
+                <input type="text" placeholder="Cerca la destinazione">
+            </div>
+
+            <div class="search-btn" role="button" id="searchBtn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#fff" class="bi bi-search"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                </svg>
+            </div>
+
+            <div class="distance-filter-section opacity-100">
+                <span>Raggio di distanza</span>
+                <input type="number" id="radius-input" value="20">
+            </div>
         </div>
 
-        <div class="search-elem">
-            <label for="guest-input">Quante persone</label>
-            <input type="text" name="guest-input" id="guest-input" placeholder="Inserisci il numero di ospiti">
-        </div>
 
-        <div class="search-elem">
-            <label for="city-input">Demo</label>
-            <input type="text" placeholder="Cerca la destinazione">
-        </div>
-
-        <div class="search-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#fff" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-            </svg>
-        </div>
     </div>
-</div>
 
-<div class="cards-container">
-    <div class="row justify-content-start align-items-start">
-        @foreach($apartments as $apartment)
-            <div class="col-12 col-lg-6 col-xxl-3">
-                <div class="personal-content">
-                    <div class="image-card-container">
-                        <img src="{{ asset("/storage/" . $apartment->image) }}" alt="{{ $apartment->name}}">
-                    </div>
-                    <div class="apartment-details">
-                        <h2 class="fw-bolder">{{ $apartment->location }}</h2>
-                        <p>{{ $apartment->description }}</p>
+    <div class="cards-container">
+        <div class="row justify-content-start align-items-start">
+            @foreach ($apartments as $apartment)
+                <div class="col-12 col-lg-6 col-xxl-3">
+                    <div class="personal-content">
+                        <div class="image-card-container">
+                            <img src="{{ asset('/storage/' . $apartment->image) }}" alt="{{ $apartment->name }}">
+                        </div>
+                        <div class="apartment-details">
+                            <h2 class="fw-bolder">{{ $apartment->location }}</h2>
+                            <p>{{ $apartment->description }}</p>
+                        </div>
                     </div>
                 </div>
-            </div> 
-        @endforeach
+            @endforeach
+        </div>
     </div>
-</div>
 
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        document.querySelector('.search-btn').addEventListener('click', function () {
+    <script>
+
+        /* FUNZIONE CHE RECUPERA LE COORDINATE DELLA CITTA' CERCATA */
+        function searchApartmentsCoordinates() {
+
+            /* INPUT DELLA CITTA' */
             const city = document.getElementById('city-input').value;
-            const guests = document.getElementById('guest-input').value; // Corretto l'ID qui
-            
-            // Esegui la richiesta Axios
-            axios.get('/api/search', {
+
+            /* CHIAMATA AXIOS PER RECUPERARE LE COORDINATE DELLA CITTA' INSERITA */
+            axios.get('https://api.tomtom.com/search/2/geocode/' + city + '.json', {
                 params: {
-                    city: city,
-                    guests: guests
+                    key: 'ARRIZGGoUek6AqDTwVcXta7pCZ07Q490'
                 }
+            }).then(function(response) {
+
+                const results = response.data.results;
+
+                /* PRENDE I PRIMI RISULTATI OTTENUTI */
+                const latitude = results[0].position.lat;
+                const longitude = results[0].position.lon;
+
+                /* PASSA I PARAMETRI LATITUDINE E LONGITUDINE ALLA FUNZIONE */
+                searchApartments(latitude, longitude);
             })
-            .then(function (response) {
-                // Aggiorna i risultati sulla pagina
-                updateResults(response.data);
-            })
-            .catch(function (error) {
-                console.error('Si è verificato un errore durante la richiesta', error);
-            });
+        }
+
+        /* FUNZIONE CHE RICERCA GLI APPARTAMENTI */
+        function searchApartments(latitude, longitude) {
+
+            /* INPUT DEL RAGGIO DI RICERCA */
+            const radius = document.getElementById('radius-input').value;
+
+            /* CHIAMATA API CON PARAMETRI LATITUDINE, LONGITUDINE E RAGGIO DI DISTANZA PER LA RICERCA */
+            axios.get('http://127.0.0.1:8000/api/apartments/', {
+                    params: {
+                        latitude: latitude,
+                        longitude: longitude,
+                        radius: radius
+                    }
+                })
+                .then(function(response) {
+                    console.log(response.data);
+
+                    const apartments = response.data.results;
+
+                    /* RECUPERO IL CONTAINER DELLE CARDS */
+                    const cardsContainer = document.querySelector('.cards-container');
+                    cardsContainer.innerHTML = '';
+
+                    /* CREO UNA ROW NEL MOMENTO DELLA RICERCA */
+                    const row = document.createElement('div');
+                    row.classList.add('row');
+
+                    /* PER OGNI APPARTAMENTO VIENE CREATA UNA CARD CON I VALORI DELLA CHIAMATA */
+                    apartments.forEach(apartment => {
+                        const card = `
+        <div class="col-12 col-lg-6 col-xxl-3">
+            <div class="personal-content">
+                <div class="image-card-container">
+                    <img src="/storage/${apartment.image}" alt="${apartment.name}">
+                </div>
+                <div class="apartment-details">
+                    <h2 class="fw-bolder">${apartment.location}</h2>
+                    <p>${apartment.description}</p>
+                </div>
+            </div>
+        </div>
+    `;
+                        row.insertAdjacentHTML('beforeend', card);
+                    });
+
+                    cardsContainer.appendChild(row);
+                })
+        }
+
+        /* AVVIA LA FUNZIONE PREMENDO L'INVIO */
+        document.getElementById('city-input').addEventListener('keyup', function(event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                searchApartments();
+            }
         });
 
-        function updateResults(results) {
-            // Aggiorna la visualizzazione dei risultati
-            const cardsContainer = document.querySelector('.cards-container .row');
-            cardsContainer.innerHTML = ''; // Svuota il contenitore delle carte
+        /* AVVIA LA FUNZIONE AL CLICK */
+        document.getElementById('searchBtn').addEventListener('click', function(event) {
+            searchApartmentsCoordinates();
+        });
 
-            results.forEach(function (apartment) {
-                const cardHTML = `
+        /* [STILE] CAMBIA L'OPACITA' (100%) PER LA SEZIONE DELLA SELEZIONE DEL RAGGIO */
+        document.getElementById('city-input').addEventListener('focus', function(event) {
+            const distanceFilterSection = document.querySelector('.distance-filter-section');
+            distanceFilterSection.style.opacity = '100';
+        });
+
+        /* [STILE] CAMBIA L'OPACITA' (0%) PER LA SEZIONE DELLA SELEZIONE DEL RAGGIO */
+        document.getElementById('city-input').addEventListener('blur', function(event) {
+            const distanceFilterSection = document.querySelector('.distance-filter-section');
+            distanceFilterSection.style.opacity = '0';
+        });
+
+        /* VERIFICA SE ALL'INVIO L'INPUT CITTA' E' VUOTO */
+        document.getElementById('city-input').addEventListener('keyup', function(event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+
+                /* SE E' VERO RISTAMPA TUTTI GLI APPARTAMENTI */
+                const city = this.value.trim(); 
+                if (city !== '') {
+                    searchApartmentsCoordinates();
+                } else {
+                    showAllApartments();
+                }
+            }
+        });
+
+
+        /* FUNZIONE CHE RISTAMPA TUTTI GLI APPARTAMENTI SE L'INPUT CITY E' VUOTO */
+        function showAllApartments() {
+
+            /* CHIAMATA AXIOS CON TUTTI GLI APPARTAMENTI */
+            axios.get('http://127.0.0.1:8000/api/apartments/')
+                .then(function(response) {
+                    const apartments = response.data.results;
+                    const cardsContainer = document.querySelector('.cards-container');
+                    cardsContainer.innerHTML = '';
+
+                    const row = document.createElement('div');
+                    row.classList.add('row');
+
+                    apartments.forEach(apartment => {
+                        const card = `
                     <div class="col-12 col-lg-6 col-xxl-3">
                         <div class="personal-content">
                             <div class="image-card-container">
-                                 <img src="/storage/${apartment.image}" alt="${apartment.name}">
+                                <img src="/storage/${apartment.image}" alt="${apartment.name}">
                             </div>
                             <div class="apartment-details">
                                 <h2 class="fw-bolder">${apartment.location}</h2>
                                 <p>${apartment.description}</p>
                             </div>
                         </div>
-                    </div>`;
-                cardsContainer.innerHTML += cardHTML;
-            });
-        }
-    });
-</script>
+                    </div>
+                `;
+                        row.insertAdjacentHTML('beforeend', card);
+                    });
 
+                    cardsContainer.appendChild(row);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }
+    </script>
 @endsection
