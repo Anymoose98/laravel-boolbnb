f92129cd918aca001150ca26d3cb65d5d226b3a9
nicolocamassa <camassanicolo03@gmail.com>
2024-03-27 10:04:50 +0100

    [UPDATE] searchbar style added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,10 +10,24 @@
                     <label for="city-input">Dove</label>
                     <input type="text" placeholder="Cerca la destinazione">
                 </div>
+
+                <div class="city-search">
+                    <label for="city-input">Quante persone</label>
+                    <input type="text" placeholder="Cerca la destinazione">
+                </div>
+
+                <div class="city-search">
+                    <label for="city-input">Demo</label>
+                    <input type="text" placeholder="Cerca la destinazione">
+                </div>
+
+                <div class="search-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#fff" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                    </svg>
+                </div>
             </div>
         </div>
-
-        
 
 
 
