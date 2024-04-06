c441971d17a95122e10d0149095e2a019348595d
nicolocamassa <camassanicolo03@gmail.com>
2024-04-06 20:46:29 +0200

    [UPDATE] style index updated

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -2,93 +2,195 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-12 mt-4">
-                <div class="d-flex justify-content-between">
-                    <div>
-                        <h2>I tuoi appartamenti</h2>
-                    </div>
+        <div class="row justify-content-start">
+            <div class="col-12">
+                <div class="window-title">
+                    <h2>I tuoi appartamenti</h2>
+                    <p>In questa pagina puoi modificare i tuoi appartamenti, sponsorizzarli o aggiungerli</p>
+                </div>
+            </div>
 
-                    <div>
-                        <a href=" {{ route('apartments.create') }} "><button class="btn btn-primary">Aggiungi un nuovo
-                                Appartamento</button></a>
-                    </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 mb-3">
+                <div class="add-apartments-button">
+                    <a class="add-btn" href="{{ route('apartments.create') }}">
+                        <div class="plus-icon">
+                            <i class="fa-solid fa-plus"></i>
+                        </div>
+                        <span>Aggiungi un nuovo appartamento</span>
+                    </a>
                 </div>
             </div>
 
             @foreach ($apartment as $apartments)
-                <div class="col-6 my-3">
-                    <div class="card">
-                        <img src="{{ asset('/storage/' . $apartments->image) }}" alt="{{ $apartments->name }}"
-                            class="card-img-top img-index">
-                        <div class="card-body">
-                            <h3 class="card-title">
-                                {{ $apartments->description }}
-                            </h3>
-                            <h5 class="card-text">{{ $apartments->location }}</h5>
-                            <a href="{{ route('apartments.show', ['apartment' => $apartments->id]) }}">
-                                <button class="btn btn-sm btn-square btn-primary">Clicca qui per maggiori
-                                    informazioni</button>
-                            </a>
+                <div class="col-xl-3 col-lg-4 col-md-6 mb-3">
+                    <div class="add-apartments-button">
+                        <div class="card-option">
+                            <div class="single-option">
+                                <a href="{{ route('apartments.edit', ['apartment' => $apartments->id]) }}"
+                                    class="text-dark">
+                                    <i class="fas fa-pen"></i>
+                                </a>
+                            </div>
+                            <div class="single-option">
+                                <a href="{{ route('sponsorship.create', ['apartment_id' => $apartments->id]) }}"
+                                    class="text-dark">
+                                    <i class="fas fa-euro"></i>
+                                </a>
+                            </div>
+                            <div class="single-option bg-danger text-white">
+                                <button class="btn btn-sm btn-square btn-danger mx-1" data-bs-toggle="modal"
+                                    data-bs-target="#modal_project_delete-{{ $apartments->id }}"
+                                    data-id="{{ $apartments->id }}" data-name="{{ $apartments->description }}"
+                                    data-type="apartments"><i class="fa-solid fa-trash"></i>
+                                </button>
+                            </div>
                         </div>
-                        <div class="d-flex position-absolute m-1 top-0 end-0">
-                            <a href="{{ route('apartments.edit', ['apartment' => $apartments->id]) }}"><button
-                                    class="btn btn-sm btn-square btn-warning "><i class="fas fa-edit"></i></button></a>
-                            <button class="btn btn-sm btn-square btn-danger mx-1" data-bs-toggle="modal"
-                                data-bs-target="#modal_project_delete-{{ $apartments->id }}"
-                                data-id= "{{ $apartments->id }}" data-name="{{ $apartments->description }}"
-                                data-type="apartments"><i class="fa-solid fa-trash"></i>
-                            </button>
-                            <a href="{{ route('sponsorship.create', ['apartment_id' => $apartments->id]) }}">Sponsorizza</a>                        </div>
+                        <a href="{{ route('apartments.show', ['apartment' => $apartments->id]) }}">
+                            <div class="img-container">
+                                <img src="{{ asset('/storage/' . $apartments->image) }}" alt="{{ $apartments->name }}"
+                                    class="card-img-top img-index">
+                            </div>
+                            <div class="apartments-info">
+                                <h2>{{ $apartments->address }}</h2>
+                                <p> {{ $apartments->description }} </p>
+                            </div>
+                        </a>
                     </div>
                 </div>
                 @include('apartments.modal_delete')
-                @endforeach
-
-            {{--  class="col-12 d-flex flex-wrap"> --}}
-
-            {{-- Tabella con le informazione relative all'appartamento --}}
-            {{-- <table class=" table mt-3 table-striped text-center">
-                    <thead>
-                        <tr>
-
-                            <th>Descrizione</th>
-                            <th>Stanze</th>
-                            <th>Letti</th>
-                            <th>Bagni</th>
-                            <th>Metri quadrati</th>
-                            <th>Zona</th>
-                            <th>Tools</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                          @foreach ($apartment as $apartments)
-                            <tr>
-                                <td>{{ $apartments->description}}</td>
-                                <td>{{ $apartments->rooms}}</td>
-                                <td>{{ $apartments->beds}}</td>
-                                <td>{{ $apartments->bathrooms}}</td>
-                                <td>{{ $apartments->square_meters}}mq</td>
-                                <td>{{ $apartments->location}}</td>
-
-                               <td> --}}
-            {{-- Bottone che rimanda alla show --}}
-            {{-- <a href="{{ route("apartments.show", ["apartment" => $apartments->id ])}}"><button class="btn btn-sm btn-square btn-primary"><i class="fas fa-eye"></i></button></a> --}}
-            {{-- Bottone che rimanda all'edit --}}
-            {{-- <a href="{{ route("apartments.edit", ["apartment" => $apartments->id ])}}"><button class="btn btn-sm btn-square btn-warning"><i class="fas fa-edit"></i></button></a> --}}
-            {{-- Bottone che richiama la modale --}}
-            {{-- <button class="btn btn-sm btn-square btn-danger" data-bs-toggle="modal" --}}
-            {{-- data-bs-target="#modal_project_delete-{{ $apartments->id }}"
-                                            data-id= "{{ $apartments->id }}" data-name="{{ $apartments->description }}" data-type="apartments">Elimina
-                                            @include("apartments.modal_delete")
-                                    </button>
-                                 </td>
-
-                            </tr>
-                            @endforeach --}}
-            {{-- </tbody> --}}
-            {{-- </table> --}}
-
+            @endforeach
         </div>
     </div>
+    </div>
+    </div>
+    </div>
+
+    <style lang="scss">
+        .apartments-info {
+            padding: 10px 15px;
+            color: black;
+
+            h2 {
+                font-size: 26px;
+                font-weight: 700;
+                margin-bottom: 0;
+            }
+
+            p {
+                color: rgb(125, 125, 125)
+            }
+
+        }
+
+        .img-container {
+            width: 100%;
+            height: calc(70%);
+            border-radius: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                object-position: center;
+                border-radius: 20px 20px 0 0;
+            }
+        }
+
+        .window-title {
+            text-align: center;
+            margin-top: 20px;
+
+            h2 {
+                font-weight: 800;
+                font-size: 40px;
+            }
+        }
+
+        .add-apartments-button:hover .card-option {
+            opacity: 1;
+        }
+
+        .add-apartments-button {
+            background-color: rgb(255, 255, 255);
+            width: 300px;
+            height: 300px;
+            border-radius: 20px;
+            position: relative;
+            -webkit-box-shadow: 0px 0px 14px 0px #00000026;
+            -moz-box-shadow: 0px 0px 14px 0px #00000026;
+            -o-box-shadow: 0px 0px 14px 0px #00000026;
+            box-shadow: 0px 0px 14px 0px #00000026;
+
+
+
+            .card-option {
+                position: absolute;
+                top: 10px;
+                right: 10px;
+                display: flex;
+                opacity: 0;
+                transition-duration: 0.3s;
+
+                .single-option {
+                    cursor: pointer;
+                    border-radius: 100px;
+                    width: 30px;
+                    height: 30px;
+                    background-color: rgb(255, 255, 255);
+                    margin-left: 10px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 14px;
+
+                    button {
+                        background-color: rgba(255, 255, 255, 0);
+                        border: none;
+                    }
+
+                    a {
+                        border: none;
+                    }
+
+                    &:hover {
+                        background-color: rgb(216, 216, 216);
+                    }
+                }
+            }
+
+            .add-btn {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                height: 100%;
+                width: 100%;
+
+                .plus-icon {
+                    width: 80px;
+                    height: 80px;
+                    background-color: black;
+                    border-radius: 100px;
+                    color: white;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 35px;
+                }
+
+                span {
+                    font-size: 20px;
+                    text-align: center;
+                    font-weight: 600;
+                    color: black;
+                    margin-top: 10px
+                }
+
+            }
+
+        }
+    </style>
 @endsection
